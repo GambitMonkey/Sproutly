f8d47b161f86e3f3376861ba6be33fcce8eb2ee8
Deniz Çağlarca <dtcaglarca@gmail.com>
2025-08-06 17:42:38 +0300

    Initial commit: Sproutly UIKit app

---
+++ Sproutly/Sproutly/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sproutly
+//
+//  Created by Deniz Turan Çağlarca on 6.08.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
