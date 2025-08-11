02105901e737e5377f02d92df0e9dc88d179fb37
Deniz Çağlarca <dtcaglarca@gmail.com>
2025-08-11 23:48:42 +0300

    chore(project): set up initial project folder structure

---
+++ Sproutly/Sproutly/Resources/Strings.swift
@@ -0,0 +1,14 @@
+//
+//  Strings.swift
+//  Sproutly
+//
+//  Created by Deniz Turan Çağlarca on 11.08.2025.
+//
+
+import Foundation
+
+struct Strings {
+    struct Onboarding {
+        static let getStartedTitle = String(localized: "plant", defaultValue: "Plant")
+    }
+}
